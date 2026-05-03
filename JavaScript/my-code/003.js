0567f54ab26adb524227774f42daf9e6cb599869
toolerdev <mrt@tooler.dev>
2026-05-02 23:29:12 -0400

    JS - exercises 001 - 004

---
+++ JavaScript/my-code/003.js
@@ -0,0 +1,11 @@
+/* You can give multiple things for console.log to print.
+Separate each thing with a comma. */
+
+console.log("Name:", 'Xander M.K.');
+console.log("Age:", 36);
+console.log("Feet Tall:", Math.floor(69 / 12));
+console.log("And Inches:", 69 - (Math.floor(69 / 12) * 12));
+console.log("Age * Height:", 36 * 69);
+console.log("Age * Feet:", 36 * Math.floor(69 / 12));
+
+// console.log("Inches Again:", 69 % 12);
---
+++ JavaScript/my-code/004.js
@@ -0,0 +1,20 @@
+// let is the best way to make a variable
+let name = 'Xander M.K.';
+let age = 36;
+let height = 69;
+
+// PUZZLE: What happens if you use Math.round?
+let feet = Math.floor(height / 12);
+// let roundedFeet = Math.round(height / 12);
+let inches = height - (feet * 12);
+
+// you can use a variable as a parameter
+console.log("Name:", name);
+console.log("Age:", age);
+
+// you can also embed variables in strings with ``
+console.log(`Height ${feet} feet ${inches} inches.`);
+
+console.log("Age * Height:", age * height);
+// you can also put math in the ${} boundaries
+console.log(`Age * Feet: ${age * feet}`);
